776e59278e4bae77884a4e3f692144ca0d395612
AVPabloCSP <a24pabloav@iessanclemente.net>
2025-11-18 20:40:03 +0100

    Add files via upload

---
+++ Codigo.py
@@ -0,0 +1,10 @@
+# x = "Hola mundo!"
+
+nome = input("Introduce o teu nome:")
+
+print("Benvido, " + nome + "!"+ "\nGrazas por pasar por aquí.")
+
+#
+# print(x)
+# print("Hola mundo!")
+# print("Hola, " + "mundo" + "!")
